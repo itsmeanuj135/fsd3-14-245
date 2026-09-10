736b0e8a33e6c48fab9fae81a104090823d104e7
anujpratap <itsmeanuj135@gmail.com>
2026-09-10 14:44:35 +0530

    lab 4 app js

---
+++ lab4/app.js
@@ -0,0 +1,57 @@
+import http from "http";
+// import * as teams from "teams.js";
+import { getAllTeams, addTeam, getTeamById } from "./teams.js";
+import { parse as parseUrl } from "url";
+
+const PORT = 5000;
+
+const sendJson = (res, statusCode, data, keyword , msg) => {
+  res.writeHead(statusCode, { "content-type": "application/json" });
+  res.end(data === "undefined" ? "" : JSON.stringify({[keyword]: msg, data}));
+};
+
+const parseJSONBody = (req) => {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        resolve(body ? JSON.parse(body) : {});
+      } catch (error) {
+        reject(error);
+      }
+    });
+    req.on("error", reject);
+  });
+};
+
+const server = http.createServer(async (req, res) => {
+  const { pathname, query } = parseUrl(req.url, true);
+  const { method } = req;
+  console.log("pathname:", pathname);
+  console.log("query:", query);
+  console.log("Method:", method);
+
+  if (pathname === "/api/v1/teams" && method === "GET") {
+    let teams = getAllTeams();
+    return sendJson(res, 200, teams, "count", teams.length);
+  } else if (pathname === "/api/v1/teams" && method == "POST") {
+    const { tname, tl, members } = await parseJSONBody(req);
+    if (!tname || !tl || !members)
+      return sendJson(400, {
+        error: "Team name, Team Leader, or member not defined",
+      });
+    const team = addTeam({ tname, tl, members });
+
+    return sendJson(res, 201, team, "Message", "Team registred succesfully");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log("SIH Server is running at ", PORT);
+});
---
+++ lab4/readme.md
@@ -0,0 +1,14 @@
+# NPM Project
+  
+  1 . create project folder 
+  2.  right lick on project folder and select open integrated terminal
+
+  3 . type in terminal `npm init -y` press enter 
+  4 open package.json file from project folder
+  5 . update type as `type:module` in package .json
+      ![alt text]()
+
+  6  type in terminal `npm i nodemon -D` to install nodemon , which restarts server while file changes
+     -D flag indicate install as dev dependency 
+     7 . it creates node_moduls folder and package-lock .json    
+     8 . update .gitignore and write projrct-folder /node_modules
