18407f6bcf15da7669b9220ac5018a7a340b8675
anujpratap <itsmeanuj135@gmail.com>
2026-09-15 09:51:52 +0530

    ran delete team by id

--- lab4/app.js
+++ lab4/app.js
@@ -1,13 +1,13 @@
 import http from "http";
-// import * as teams from "teams.js";
 import { getAllTeams, addTeam, getTeamById } from "./teams.js";
 import { parse as parseUrl } from "url";
+import { deleteTeam } from "./teams.js";
 
 const PORT = 5000;
 
-const sendJson = (res, statusCode, data, keyword , msg) => {
+const sendJson = (res, statusCode, data, keyword, msg) => {
   res.writeHead(statusCode, { "content-type": "application/json" });
-  res.end(data === "undefined" ? "" : JSON.stringify({[keyword]: msg, data}));
+  res.end(data === "undefined" ? "" : JSON.stringify({ [keyword]: msg, data }));
 };
 
 const parseJSONBody = (req) => {
@@ -35,22 +35,55 @@ const server = http.createServer(async (req, res) => {
   console.log("Method:", method);
 
   if (pathname === "/api/v1/teams" && method === "GET") {
+
     let teams = getAllTeams();
     return sendJson(res, 200, teams, "count", teams.length);
-  } else if (pathname === "/api/v1/teams" && method == "POST") {
+
+  } 
+  
+  else if (pathname === "/api/v1/teams" && method == "POST") {
+
     const { tname, tl, members } = await parseJSONBody(req);
     if (!tname || !tl || !members)
-      return sendJson(400, {
-        error: "Team name, Team Leader, or member not defined",
+      return sendJson(res, 400, {
+        error: "Team Name, Team Leader, or Members not defined",
       });
     const team = addTeam({ tname, tl, members });
 
-    return sendJson(res, 201, team, "Message", "Team registred succesfully");
-  } else {
+    return sendJson(res, 201, team, "Message", "Team registered successfully");
+
+  } 
+  
+  
+  
+  else if (pathname.startsWith("/api/v1/teams/") && method === "GET") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 400, {
+        error: `Team with id: ${id} not found`,
+      });
+    return sendJson(res, 200, team, "Message", "Team Found");
+  }
+  else if (pathname.startsWith("/api/v1/teams/") && method === "DELETE") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 400, {
+        error: `Team with id: ${id} not found`,
+      });
+      deleteTeam(id);
+    return sendJson(res, 200, team, "Message", "Team deleted");
+  }
+  else {
     res.statusCode = 404;
-    res.end();
+    res.end("Not matching");
   }
 });
+ 
+ 
 
 server.listen(PORT, () => {
   console.log("SIH Server is running at ", PORT);
