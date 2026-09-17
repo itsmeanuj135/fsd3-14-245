693f1c68dda66bc7e8ff8934a1819715b3629cb2
anujpratap <itsmeanuj135@gmail.com>
2026-09-17 13:41:26 +0530

    ran the express server

--- lab5/app.js
+++ lab5/app.js
@@ -2,10 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.end("<h1>Hello Express</h1>");
+});
 app.get("/about", (req, res) => {
-    res.end("<h1>Hello Express</h1>")
+    res.end("<h1>WE are FSD Developer</h1>");
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.use((req, res) => {
+  res.status(404).send("NOT FOUND");
 });
+app.listen(3333, () => {
+  console.log('Server is running on port 3333');
+});
+ 
---
+++ lab5/readme.md
@@ -0,0 +1,17 @@
+# Express
+Fast, unopinionated, minimalist web framework for Node.js
+
+1. create npm project  `npm init -y`
+update package .json
+`type: module`
+3. install nodemon with `npm i nodemon -D`
+4 . install express with `npm i express`
+5. update package.json 
+```
+"scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  ```
+ 
+  
