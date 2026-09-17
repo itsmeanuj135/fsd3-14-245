d3439418c0bc4076733ff9045fa91c314d35e752
anujpratap <itsmeanuj135@gmail.com>
2026-09-17 13:59:42 +0530

    basic routing with express

--- lab5/app.js
+++ lab5/app.js
@@ -8,9 +8,23 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.end("<h1>WE are FSD Developer</h1>");
 });
+
+
+app.post("/login",(req, res) => {
+  res.send({msg: 'user login'});
+});
+app.put("/user/update/1",(req, res) => {
+  res.send({msg: "user updated"});
+});
+app.delete("/user/1",(req, res) => {
+  res.send({msg: "REMOVE USER 1"});
+});
 app.use((req, res) => {
   res.status(404).send("NOT FOUND");
 });
+
+
+
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
 });
